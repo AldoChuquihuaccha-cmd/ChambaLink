1cac027f6ad1cf5061ab588ade2553745b920640
Pieroland <pieropaolobobadillabernedopier@gmail.com>
2026-09-20 17:28:03 -0500

    Modificacion Certificacion class

--- ChambaLink/Certificacion.h
+++ ChambaLink/Certificacion.h
@@ -1,29 +1,66 @@
 #pragma once
 #include <string>
 
+
 class Certificacion {
+
 private:
+
     int id;
     std::string nombre;
     std::string institucion;
     std::string fechaObtencion;
     std::string codigo;
 
+
 public:
-    Certificacion();
+
+
+    Certificacion()
+        : id(0), nombre(""), institucion(""),
+          fechaObtencion(""), codigo("") {}
+
+
 
     Certificacion(int pId,
                   std::string pNombre,
                   std::string pInstitucion,
                   std::string pFecha,
-                  std::string pCodigo);
+                  std::string pCodigo)
+        : id(pId),
+          nombre(pNombre),
+          institucion(pInstitucion),
+          fechaObtencion(pFecha),
+          codigo(pCodigo) {}
 
-    int getId() const;
 
-    std::string getNombre() const;
-    std::string getInstitucion() const;
-    std::string getFechaObtencion() const;
-    std::string getCodigo() const;
 
-    bool mismoId(const Certificacion& otra) const;
+    int getId() const {
+        return id;
+    }
+
+
+    std::string getNombre() const {
+        return nombre;
+    }
+
+
+    std::string getInstitucion() const {
+        return institucion;
+    }
+
+
+    std::string getFechaObtencion() const {
+        return fechaObtencion;
+    }
+
+
+    std::string getCodigo() const {
+        return codigo;
+    }
+
+
+    bool mismoId(const Certificacion& otra) const {
+        return id == otra.id;
+    }
 };
