e85d1b18cf3051cfa0f76e3775f3e2047dc894ce
Joaosito <joaorivero27@gmail.com>
2026-09-20 17:08:37 -0500

    Eliminacion de Cpps innecsarios

--- ChambaLink/Accion.h
+++ ChambaLink/Accion.h
@@ -24,3 +24,22 @@ public:
     std::string getDescripcion() const;
     bool fueEjecutada() const;
 };
+
+Accion::Accion() : descripcion(""), hacer(nullptr), deshacer(nullptr), ejecutada(false) {}
+
+Accion::Accion(std::string pDescripcion, std::function<void()> pHacer, std::function<void()> pDeshacer)
+    : descripcion(pDescripcion), hacer(pHacer), deshacer(pDeshacer), ejecutada(false) {
+}
+
+void Accion::ejecutar() {
+    if (hacer) hacer();
+    ejecutada = true;
+}
+
+void Accion::deshacerAccion() {
+    if (deshacer) deshacer();
+    ejecutada = false;
+}
+
+std::string Accion::getDescripcion() const { return descripcion; }
+bool Accion::fueEjecutada() const { return ejecutada; }
