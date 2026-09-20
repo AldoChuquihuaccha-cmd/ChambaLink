c2dcbba10be327ed265e693b6d35df9345c13407
Pieroland <pieropaolobobadillabernedopier@gmail.com>
2026-09-20 17:27:36 -0500

    Modificacion GrupoProfesional class

--- ChambaLink/GrupoProfesional.h
+++ ChambaLink/GrupoProfesional.h
@@ -1,31 +1,75 @@
 #pragma once
 #include <string>
 
+
 class GrupoProfesional {
+
 private:
+
     int id;
     std::string nombre;
     std::string descripcion;
     std::string especialidad;
     int cantidadMiembros;
 
+
 public:
-    GrupoProfesional();
+
+
+    GrupoProfesional()
+        : id(0), nombre(""), descripcion(""),
+          especialidad(""), cantidadMiembros(0) {}
+
 
     GrupoProfesional(int pId, std::string pNombre,
                      std::string pDescripcion,
-                     std::string pEspecialidad);
+                     std::string pEspecialidad)
+        : id(pId),
+          nombre(pNombre),
+          descripcion(pDescripcion),
+          especialidad(pEspecialidad),
+          cantidadMiembros(0) {}
 
-    int getId() const;
 
-    std::string getNombre() const;
-    std::string getDescripcion() const;
-    std::string getEspecialidad() const;
 
-    int getCantidadMiembros() const;
+    int getId() const {
+        return id;
+    }
 
-    void agregarMiembro();
-    void eliminarMiembro();
 
-    bool mismoId(const GrupoProfesional& otro) const;
+    std::string getNombre() const {
+        return nombre;
+    }
+
+
+    std::string getDescripcion() const {
+        return descripcion;
+    }
+
+
+    std::string getEspecialidad() const {
+        return especialidad;
+    }
+
+
+    int getCantidadMiembros() const {
+        return cantidadMiembros;
+    }
+
+
+    void agregarMiembro() {
+        cantidadMiembros++;
+    }
+
+
+    void eliminarMiembro() {
+
+        if(cantidadMiembros > 0)
+            cantidadMiembros--;
+    }
+
+
+    bool mismoId(const GrupoProfesional& otro) const {
+        return id == otro.id;
+    }
 };
